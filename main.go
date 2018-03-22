408c1860df293fb68d88e060fc5abe24581e5d1d
Andrey <folganoid@yahoo.com>
2018-03-22 23:05:44 +0200

    some fixes

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"./models"
 	"fmt"
+	"html/template"
 	"net/http"
 )
 
@@ -40,16 +41,19 @@ func editHandler(rnd render.Render, r *http.Request, params martini.Params) {
 func savePostHandler(rnd render.Render, r *http.Request) {
 	id := r.FormValue("id")
 	title := r.FormValue("title")
-	content := r.FormValue("content")
+	contentMarkdown := r.FormValue("content")
+
+	contentHtml := string(blackfriday.MarkdownBasic([]byte(contentMarkdown)))
 
 	var post *models.Post
 	if id != "" {
 		post = posts[id]
 		post.Title = title
-		post.Content = content
+		post.ContentHtml = contentHtml
+		post.ContentMarkdown = contentMarkdown
 	} else {
 		id = GenerateId()
-		post := models.NewPost(id, title, content)
+		post := models.NewPost(id, title, contentHtml, contentMarkdown)
 		posts[post.Id] = post
 	}
 
@@ -73,6 +77,10 @@ func getHtmlHandler(rnd render.Render, r *http.Request) {
 	rnd.JSON(200, map[string]interface{}{"html": string(htmlBytes)})
 }
 
+func unescape(x string) interface{} {
+	return template.HTML(x)
+}
+
 func main() {
 	fmt.Println("Listening on port :3000")
 
@@ -80,13 +88,15 @@ func main() {
 
 	m := martini.Classic()
 
+	unescapeFuncMap := template.FuncMap{"unescape": unescape}
+
 	m.Use(render.Renderer(render.Options{
-		Directory:  "templates",                // Specify what path to load the templates from.
-		Layout:     "layout",                   // Specify a layout template. Layouts can call {{ yield }} to render the current template.
-		Extensions: []string{".tmpl", ".html"}, // Specify extensions to load for templates.
-		//Funcs: []template.FuncMap{AppHelpers}, // Specify helper function maps for templates to access.
-		Charset:    "UTF-8", // Sets encoding for json and html content-types. Default is "UTF-8".
-		IndentJSON: true,    // Output human readable JSON
+		Directory:  "templates",                         // Specify what path to load the templates from.
+		Layout:     "layout",                            // Specify a layout template. Layouts can call {{ yield }} to render the current template.
+		Extensions: []string{".tmpl", ".html"},          // Specify extensions to load for templates.
+		Funcs:      []template.FuncMap{unescapeFuncMap}, // Specify helper function maps for templates to access.
+		Charset:    "UTF-8",                             // Sets encoding for json and html content-types. Default is "UTF-8".
+		IndentJSON: true,                                // Output human readable JSON
 	}))
 
 	staticOptions := martini.StaticOptions{Prefix: "assets"}
